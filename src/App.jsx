4f26b04676d1610e37cde4291a3ad297f520ae74
sambulo <sambulosendas@gmail.com>
2018-12-02 15:42:35 +0000

    Added react-bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,43 @@
 import React, { Component } from "react";
+import "./App.css";
+import { FormGroup, FormControl, InputGroup, Glyphicon } from "react-bootstrap";
+
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state ={
+     query: ''
+    }
+  }
+  search(){
+    console.log('this.state', this.state);
+  }
+
   render() {
     return (
-      <div>
+      <div className="App">
         <div className="App-title">Music Mix</div>
-        <div>
-          <input placeholder="Search your favourate artist" />
-          <button>Button</button>
-        </div>
+        <FormGroup>
+          <InputGroup>
+            <FormControl 
+            type="text"
+            placeholder="Search for an Artist"
+            value={this.state.query}
+            onChange={event => {this.setState({query: event.target.value})}}
+
+            onKeyPress={event => {
+              if (event.key === 'Enter'){
+              this.search()
+              }
+            }}
+            />
+            <InputGroup.Addon onClick={() =>this.search()}>
+              <Glyphicon glyph="search"></Glyphicon>
+            </InputGroup.Addon>
+          </InputGroup>
+        </FormGroup>
         <div className="Profile">
           <div>Artist Picture</div>
           <div>Artist Name</div>
@@ -18,4 +47,5 @@ class App extends Component {
     );
   }
 }
+
 export default App;
