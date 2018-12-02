a2d54b25537482052b55ad48390b339c15c64146
sambulo <sambulosendas@gmail.com>
2018-12-02 21:10:16 +0000

    Added A client-side JS wrapper for the Spotify Web API

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import { FormGroup, FormControl, InputGroup, Glyphicon } from "react-bootstrap";
+const Sportify = require(spotify-web-api-js);
 
 
 class App extends Component {
